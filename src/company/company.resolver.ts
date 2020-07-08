6b32ae2fc3a79e7d8ee8c0d2f5f9558e1f34f934
Tibbar <851207725@qq.com>
2020-07-08 15:18:01 +0800

    添加呢查询所有信息接口

--- src/company/company.resolver.ts
+++ src/company/company.resolver.ts
@@ -3,16 +3,17 @@ import { Int } from "type-graphql";
 import { CompanyService } from "./company.service";
 import { CompanyEntity } from './company.entity';
 import { CompanyInput } from './company.input';
+import { query } from 'express';
 
 @Resolver('Company')
 export class CompanyResolver {
     constructor(
         private companyService: CompanyService
     ) { }
-    
+
     @Query(returns => CompanyEntity, { name: "getCompanyInfoById", description: "使用 ID 获取公司信息" })
     async getCompanyInfoById(
-        @Args({ name: 'companyId' , type: () => Int, nullable: false }) companyId: number
+        @Args({ name: 'companyId', type: () => Int, nullable: false }) companyId: number
     ): Promise<CompanyEntity> {
         return await this.companyService.getCompanyInfoById(companyId);
     }
@@ -20,14 +21,19 @@ export class CompanyResolver {
     @Mutation(returns => Boolean, { name: 'updateCompanyInfo', description: "更新或插入信息，输入中不含 ID 或者查无此 ID 时新建记录，否则修改已有记录" })
     async updateCompanyInfo(
         @Args({ name: 'companyInput', type: () => CompanyInput, nullable: false }) companyInput: CompanyInput
-    ) {
+    ): Promise<Boolean> {
         return await this.companyService.updateCompanyInfo(companyInput);
     }
 
     @Mutation(returns => Boolean, { name: 'deleteCompanyById', description: "删除公司信息" })
     async deleteCompanyById(
         @Args({ name: 'companyId', type: () => Int, nullable: false }) companyId: number
-    ) {
+    ): Promise<Boolean> {
         return await this.companyService.deleteCompanyById(companyId);
+    }
+
+    @Query(returns => [CompanyEntity], { name: 'getAllCompanyInfo', description: "获取所有公司信息" })
+    async getAllCompanyInfo(): Promise<CompanyEntity[]> {
+        return await this.companyService.getAllCompanyInfo();
     }
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -38,4 +38,8 @@ export class CompanyService {
         }
         return true;
     }
+
+    async getAllCompanyInfo(): Promise<CompanyEntity[]> {
+        return await this.companyRepository.find();
+    }
 }
